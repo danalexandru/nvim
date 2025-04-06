d9ebe4109872c353374713a54a10a01a9540f036
aldan <alexandrubogdan.dan@asml.com>
2025-04-06 13:15:50 +0300

    update lsp config and keybindings

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -20,4 +20,13 @@ return {
     },
     {"lervag/vimtex"},
     {"dkarter/bullets.vim"},
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    },
 }
--- lua/plugins/ranger.lua
+++ lua/plugins/ranger.lua
@@ -1,5 +1,18 @@
 return {
     -- Shortened Github Url
     "francoiscabrol/ranger.vim",
-    vim.keymap.set("n", "<C-p>", ":Ranger<cr>", { desc = "Open Ranger file manager" }),
+    vim.keymap.set("n", "<C-p>", ":Ranger<CR>:bd<CR>", { desc = "Open Ranger file manager" }),
 }
+
+-- return {
+--   "kelly-lin/ranger.nvim",
+--   config = function()
+--     require("ranger-nvim").setup({ replace_netrw = true })
+--     vim.api.nvim_set_keymap("n", "<C-p>", "", {
+--       noremap = true,
+--       callback = function()
+--         require("ranger-nvim").open(true)
+--       end,
+--     })
+--   end,
+-- }
