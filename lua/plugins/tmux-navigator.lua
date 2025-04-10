a0cf667c4bec6bc6674663829e08e61bf1c37742
aldan <alexandrubogdan.dan@asml.com>
2025-04-10 17:01:07 +0300

    make sure navigation extends to only left/right and not up/down

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -5,7 +5,14 @@ return {
     "TmuxNavigateRight",
   },
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
   },
+  config = function()
+    vim.g.tmux_navigator_no_mappings = 1
+    -- vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigatorLeft<CR>", { desc = "Move focus to the left window" })
+    -- vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigatorRight<CR>", { desc = "Move focus to the right window" })
+    vim.keymap.set("n", "<C-j>", "3j3<C-e>", { desc = "Scroll 3 lines down" })
+    vim.keymap.set("n", "<C-k>", "3k3<C-y>", { desc = "Scroll 3 lines down" })
+  end
 }
